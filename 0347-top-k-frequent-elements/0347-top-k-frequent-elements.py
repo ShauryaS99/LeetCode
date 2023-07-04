9e21a0ae74a6d61ddcc3b75862d38aa8c0dd4990
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2023-07-03 19:40:07 -0700

    Time: 124 ms (31.67%), Space: 21.1 MB (58.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,14 @@
 import heapq as hq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
+        freq = {} # create frequency hashmap (unique keys)
         for i in nums:
-            freq[i] = freq.get(i, 0) - 1
+            freq[i] = freq.get(i, 0) - 1 #keep frequencies as negative so easy use of minHeap
         heap = []
         for key, value in freq.items():
-            hq.heappush(heap, (value, key))
+            hq.heappush(heap, (value, key)) #insert tuples of (frequency, key) so minheap can operate on frequency
         res = []
         while len(res) < k:
-            res.append(hq.heappop(heap)[1])
+            res.append(hq.heappop(heap)[1]) #pop k values, we care about the key
         return res
         
