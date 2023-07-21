f0994884ca43a90d0ffbcb29a9e0a0b8cc825bcd
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2023-07-21 12:18:41 -0700

    Time: 60 ms (60.07%), Space: 18.9 MB (57.70%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,11 +6,14 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        #need to pass bounds because of case [5,4,6,null,null,3,7] 
         def isValidBSTHelper(root, lower_bound, upper_bound):
             if not root:
                 return True
+            #if root.val is outside of bounds return false
             if (root.val >= upper_bound or root.val <= lower_bound):
                 return False
+            #if all conditions succeed keep recursing
             return (isValidBSTHelper(root.left, lower_bound, root.val) and isValidBSTHelper(root.right, root.val, upper_bound))
         return isValidBSTHelper(root, float('-inf'), float('inf'))
     
