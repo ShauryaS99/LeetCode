d53ac4121709f37b6d9262719cf92eaa77638d6e
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2023-07-26 17:34:24 -0700

    Time: 122 ms (99.66%), Space: 17.2 MB (75.87%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        stack = []
+        visited = set()
+        max_area = 0
+        
+        def explore_directions(x,y):
+            directions = []
+            north = (x -1, y)
+            south = (x + 1, y)
+            east = (x, y + 1)
+            west = (x, y - 1)
+            if x - 1 >= 0:
+                directions.append(north)
+            if x + 1 < len(grid):
+                directions.append(south)
+            if y + 1 < len(grid[0]):
+                directions.append(east)
+            if y - 1 >= 0:
+                directions.append(west)
+            return directions
+                
+            
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0 or (i,j) in visited:
+                    continue
+                stack.append((i,j))
+                curr_area = 0
+                while stack:
+                    x,y = stack.pop()
+                    if grid[x][y] == 0 or (x,y) in visited:
+                        continue
+                    if (x,y) not in visited:
+                        visited.add((x,y))
+                        stack.extend(explore_directions(x,y))
+                    curr_area += 1
+                max_area = max(max_area, curr_area)
+                    
+        return max_area
+    
