24df74320649150a16d9e08a567d1b5f26c1bc45
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2023-08-02 16:49:22 -0700

    Time: 278 ms (90.47%), Space: 17.7 MB (96.23%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,14 +1,14 @@
 import heapq as hq
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        #Heap approach
+        #Heap approach O(nlogk) b/c heap is only size k
         res = []
         for i in range(k):
-            hq.heappush(res, arr[i])
+            hq.heappush(res, arr[i]) #push preliminary k items
         for i in range(k, len(arr)):
-            if abs(x - arr[i]) < abs(x - res[0]):
+            if abs(x - arr[i]) < abs(x - res[0]): #only if distances is closer do we replace the elements
                 hq.heappop(res)
                 hq.heappush(res, arr[i])
-        res.sort()
+        res.sort() #sort list 
         return res
             
