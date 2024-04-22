129b100102a380feee743b150bfaf668a1aa342a
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-04-22 00:01:50 -0700

    Time: 24 ms (98.03%), Space: 16.4 MB (80.97%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -6,23 +6,11 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # left subtree, right subtree, root
-        res = []
-        stack = []
-        curr = root
-        visited = set()
-        #process node once you confirm the right child has been visited
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                visited.add(curr)
-                curr = curr.left
-            curr = stack[len(stack) - 1]
-            #apped res and wait to be popped up a level
-            if not curr.right or curr.right in visited:
-                curr = stack.pop()
-                res.append(curr.val)
-                curr = None #don't want to go right again b/c already visisted
-            else:
-                curr = curr.right
-        return res
+        postorder = []
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                dfs(node.right)
+                postorder.append(node.val)
+        dfs(root)
+        return postorder
