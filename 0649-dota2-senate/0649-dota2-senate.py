363b053d73532fc3effad0146df0a8848ff682c3
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-05-16 13:36:43 -0700

    Time: 44 ms (84.91%), Space: 17.2 MB (5.11%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,33 +1,19 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        senate_lst = list(senate)
-        victory = False
-        # Time complexity O(n^2)
-        while not victory:
-            s = 0
-            while s < len(senate_lst):
-                next_s = (s + 1) % len(senate_lst)
-                # Radiant senator will block the next Dire
-                if senate_lst[s] == "R":
-                    while senate_lst[next_s] != "D":
-                        # No remaining Dire
-                        if next_s == s:
-                            return "Radiant"
-                        next_s += 1
-                        if next_s >= len(senate_lst):
-                            next_s = next_s % len(senate_lst)
-                    senate_lst[next_s] = 0
-                # Dire senator will block the next Radiant
-                elif senate_lst[s] == "D":
-                    while senate_lst[next_s] != "R":
-                        # No remaining Radiant
-                        if next_s == s:
-                            return "Dire"
-                        next_s += 1
-                        if next_s >= len(senate_lst):
-                            next_s = next_s % len(senate_lst)
-                    senate_lst[next_s] = 0
-                s += 1
-        return "Error"
+        radiant = deque([i for i in range(len(senate)) if senate[i] == "R"])
+        dire = deque([i for i in range(len(senate)) if senate[i] == "D"])
+        
+        # 2 queue approach O(N)
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+            if r < d:
+                radiant.append(r + len(senate))
+            else:
+                dire.append(d + len(senate))
+        if radiant:
+            return "Radiant"
+        else:
+            return "Dire"
                         
             
