c0ce0e345a454b188c34d702ad74987b9be71a7f
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-06-07 22:50:00 -0700

    Time: 95 ms (27.01%), Space: 18.4 MB (29.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #Initialize adjacency list
-        prereq_dict = {k:[] for k in range(numCourses)}
+        # Initialize adjacency list
+        prereq_dict = {k: [] for k in range(numCourses)}
         for crs, pre in prerequisites:
             prereq_dict[crs].append(pre)
         
         visited = set()
+        rec_stack = set()
         
-        #Cycle detection: if node in visited set already
+        # Cycle detection: if node in visited set already
         def dfs(crs):
-            if crs in visited:
+            if crs in rec_stack:
                 return False
-            if prereq_dict[crs] == []:
+            if crs in visited:
                 return True
             
-            visited.add(crs)
+            rec_stack.add(crs)
             for pre in prereq_dict[crs]:
                 if not dfs(pre):
                     return False
-            visited.remove(crs)
-            prereq_dict[crs] = []
+            rec_stack.remove(crs)
+            visited.add(crs)
             return True
             
-            
-        # O(n^2) space complexity: O(n)
         for crs in range(numCourses):
-            visited = set()
             if not dfs(crs):
                 return False
         return True
