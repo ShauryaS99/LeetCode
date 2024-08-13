52fb3f3e85a58d02e8b6eae2f66967956db2f7d8
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-08-12 20:22:11 -0700

    Time: 70 ms (89.00%), Space: 20 MB (15.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -6,21 +6,25 @@
 import heapq as hq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # O(nlogn)
-        # Aggregate all linked lists into a min heap
-        agg_lst = []
-        for node in lists:
-            while node:
-                agg_lst.append(node.val)
-                node = node.next
-        hq.heapify(agg_lst)
+        # O(nlogk)
+        # Initialize heap with first values of k lists
+        merged_heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                hq.heappush(merged_heap, (lists[i].val, i))
         
-        # Recreate linked list from min heap
-        head = curr = ListNode(0)
-        while agg_lst:
-            elem = hq.heappop(agg_lst)
-            curr.next = ListNode(elem)
+        # Iteratively add K possible next nodes, and heappop the next one 
+        curr = dummy = ListNode(0)
+        while merged_heap:
+            min_elem, curr_list = hq.heappop(merged_heap)
+            # Set next to curr_lst, advance that list and add element to heap
+            curr.next = lists[curr_list]
             curr = curr.next
+            lists[curr_list] = lists[curr_list].next
+            if curr.next:
+                hq.heappush(merged_heap, (curr.next.val, curr_list))
         
-        return head.next
+        return dummy.next
+            
+        
         
