96b6a1237c60922b60e42b875338dc87981b2247
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-09-13 19:00:52 -0700

    Time: 174 ms (35.15%), Space: 21.2 MB (49.31%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,21 +1,28 @@
 import random
 class Solution:
-#O(n) for init and pickIndex
+#O(n) for init and O(logn) for pickIndex
     def __init__(self, w: List[int]):
         self.sum = sum(w)
-        # window = index
-        self.weight_dict = {}
-        curr_max = 0
+        # Each index represents a range of values out of the total sum
+        # if the randint falls in the range of values => pick that idx
+        self.prefix_sum = []
+        curr_sum = 0
         for idx in range(len(w)):
-            curr_max += w[idx]
-            self.weight_dict[curr_max] = idx
+            curr_sum += w[idx]
+            self.prefix_sum.append(curr_sum)
         
 
     def pickIndex(self) -> int:
         random_num = random.randint(1, self.sum)
-        for max_num, idx in self.weight_dict.items():
-            if random_num <= max_num:
-                return idx
+        # Binary search
+        left, right = 0, len(self.prefix_sum)
+        while left <= right:
+            mid = (left + right) // 2
+            if random_num > self.prefix_sum[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
 
 
 # Your Solution object will be instantiated and called as such:
