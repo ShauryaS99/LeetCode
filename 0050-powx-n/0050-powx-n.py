15025cfca7eafded8a33e45ea39b441709d5aeca
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-09-16 00:34:49 -0700

    Time: 23 ms (98.71%), Space: 16.5 MB (85.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,11 @@
-#what ...
+# binary exponentiation
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        if n == 0:
+            return 1.0
+        if n < 0:
+            return 1.0 / self.myPow(x, -n)
+        if n %2 == 1:
+            return x * self.myPow(x * x, (n - 1) // 2)
+        if n % 2 == 0:
+            return self.myPow(x * x, n // 2)
