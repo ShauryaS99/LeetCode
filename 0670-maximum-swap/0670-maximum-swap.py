f6a71f09019d99a38a9b5d3e5dea66826435cd5e
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-10-08 00:31:45 -0700

    Time: 44 ms (5.37%), Space: 16.6 MB (8.38%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,21 +1,28 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        #O(n^2) runtime
-        num = str(num)
-        swap = False
-        for i in range(len(num) - 1):
-            digit = num[i]
-            max_dig = i
-            for j in range(i + 1, len(num)):
-                if num[j] > num[max_dig]:
-                    max_dig = j
-                    swap = True
-                if num[j] == num[max_dig] and swap:
-                    max_dig = j
-                    swap = True
-            if swap:
-                num = list(num)
-                num[i], num[max_dig] = num[max_dig], num[i]
-                num = ''.join(num)
-                return int(num)
-        return int(num)
+        #O(n) runtime; O(n) space
+        num_lst = list(str(num))
+        max_lst = [-1] * len(num_lst)
+        
+        # Iterate from right, finding max number from the right till that index
+        # We do this to swap with the rightmost (lowest value) higest number
+        curr_max = -1
+        curr_max_idx = -1
+        for i in range(len(num_lst) - 1, -1, -1):
+            curr = int(num_lst[i])
+            if curr > curr_max:
+                curr_max = curr
+                curr_max_idx = i
+            max_lst[i] = (curr_max, curr_max_idx)
+        
+        # Iterate from left, finding first number that can benefit from swap
+        # Find num < max => swap
+        for i in range(len(num_lst)):
+            # Greater number to right exists => swap
+            if max_lst[i][0] > int(num_lst[i]):
+                max_index = max_lst[i][1]
+                num_lst[i], num_lst[max_index] = num_lst[max_index], num_lst[i]
+                break
+            
+        return int("".join(num_lst))
+        
