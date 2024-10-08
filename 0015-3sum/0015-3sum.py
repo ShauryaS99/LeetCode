45c037dd326c3087431979b98ad342552bcb9f90
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-10-08 11:40:10 -0700

    Time: 1833 ms (8.15%), Space: 20.4 MB (87.14%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #O(nlogn + n^2)
         res = set()
         nums.sort()
-        for idx, val in enumerate(nums):
-            l = idx +1
+        prev = -float('inf')
+        for i in range(len(nums) - 2):
+            l = i + 1
             r = len(nums) - 1
-            target = 0 - val
             while l < r:
-                if nums[l] + nums[r] > target:
-                    r -= 1
-                elif nums[l] + nums[r] < target:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.add((nums[i], nums[l], nums[r]))
                     l += 1
+                    r -= 1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
                 else:
-                    res.add((val, nums[l], nums[r]))
                     l += 1
-                    r -= 1
         return res
