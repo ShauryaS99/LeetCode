240540745defdd4d4cd3b92ff6a95f2a2f79d89b
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-10-09 09:54:31 -0700

    Time: 36 ms (47.39%), Space: 16.6 MB (26.91%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -5,23 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    #rules
-    #go left as much as possible
-    
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        stack = []
-        curr = root
-        # you need stack or curr b/c stack is empty once you go to right subtree
-        while stack or curr:
-            # go all the way left until it is null
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            # get the last on the stack --> process and go right
-            curr = stack.pop()
-            res.append(curr.val)
-            curr = curr.right
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                res.append(node.val)
+                dfs(node.right)
+        
+        dfs(root)
         return res
-            
-            
