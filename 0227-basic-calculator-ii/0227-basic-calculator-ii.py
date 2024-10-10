d2680a0cb34f0f54f2d6e0d44aab11b38b807dd8
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-10-09 21:08:15 -0700

    Time: 141 ms (24.01%), Space: 19.2 MB (83.07%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,43 +1,34 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        i = 0
-        prev = res = 0
+        res = prev = 0
         operation = "+"
-        
+        i = 0
         while i < len(s):
-            # Skip spaces
-            if s[i] == " ":
+            while i < len(s) and s[i] == " ":
                 i += 1
-                continue
-            
-            curr_num = ""
-            # Read the entire number, skipping spaces if necessary
+            curr_digit = ""
             while i < len(s) and s[i].isdigit():
-                curr_num += s[i]
+                curr_digit += s[i]
                 i += 1
+            curr_digit = int(curr_digit)
             
-            if curr_num:
-                curr_num = int(curr_num)
-                
-                # Perform the operation
-                if operation == "+":
-                    res += curr_num
-                    prev = curr_num  # Update prev to current number
-                elif operation == "-":
-                    res -= curr_num
-                    prev = -curr_num  # Update prev as negative for subtraction
-                elif operation == "*":
-                    res -= prev  # Remove prev from result
-                    res += prev * curr_num  # Add updated value
-                    prev = prev * curr_num  # Update prev
-                elif operation == "/":
-                    res -= prev  # Remove prev from result
-                    res += int(prev / curr_num)  # Add updated value (integer division)
-                    prev = int(prev / curr_num)  # Update prev
-            
-            # Update the operation (make sure to skip spaces again)
-            if i < len(s) and s[i] != " ":
+            if operation == "+":
+                res += curr_digit
+                prev = curr_digit
+            elif operation == "-":
+                res -= curr_digit
+                prev = -curr_digit
+            elif operation == "*":
+                res -= prev
+                res += prev * curr_digit
+                prev = prev * curr_digit
+            else:
+                res -= prev
+                res += int(prev / curr_digit)
+                prev = int(prev / curr_digit)
+            while i < len(s) and s[i] == " ":
+                i += 1
+            if i < len(s) and not s[i].isdigit():
                 operation = s[i]
             i += 1
-
         return res
