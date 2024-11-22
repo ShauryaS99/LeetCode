89c763a49b5ebd745fa2a6ffd8317f5d2bd22396
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-11-21 21:10:03 -0800

    Time: 11 ms (32.07%), Space: 20.9 MB (19.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        #O(nlogn)
-        sorted_intervals = deque(sorted(intervals))
-        new_intervals = deque([])
-        prev = [-1, -1]
-        while sorted_intervals:
-            nxt = sorted_intervals.popleft()
-            start, end = nxt[0], nxt[1]
-            if start > prev[1]:
-                new_intervals.append(nxt)
-                prev = nxt
-            else:
-                if nxt[1] > prev[1]:
-                    new_intervals.pop()
-                    new_intervals.append([prev[0], nxt[1]])
-                    prev = [prev[0], nxt[1]]            
-        return new_intervals
-            
+        intervals.sort()
+        res = []
+        prev_start, prev_end = intervals[0][0], intervals[0][1]
+        for interval in intervals:
+            start, end = interval
+            if start > prev_end:
+                res.append([prev_start, prev_end])
+                prev_start = start
+                prev_end = end
+            elif start <= prev_end:
+                prev_end = max(prev_end, end)
+        res.append([prev_start, prev_end])
+        return res
