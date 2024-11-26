8102212f6f3e2eec56584193bb7875e41d5119a5
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-11-25 23:16:04 -0800

    Time: 3 ms (81.70%), Space: 16.6 MB (86.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,40 +1,35 @@
+from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        q = deque()
-        visited = set()
-        #Add all rotten oranges to deque
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 2:
-                    q.append((row, col))
-                    visited.add((row, col))
+        q = deque([])
+        day = 0
+        # initialize visited set
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    q.append((i, j))
         
-        # Run BFS from multiple sources
-        num_mins = 0
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
         while q:
             size = len(q)
-            #For a given level: expand outwards
             for _ in range(size):
-                curr = q.popleft()
-                dirs = [(1,0), (-1, 0), (0, 1), (0, -1)]
+                x, y = q.popleft()
                 for dx, dy in dirs:
-                    x = curr[0] + dx
-                    y = curr[1] + dy
-                    if x not in range(rows) or y not in range(cols):
-                        continue
-                    if grid[x][y] == 0 or (x,y) in visited:
-                        continue
-                    if grid[x][y] == 1:
-                        grid[x][y] = 2
-                    q.append((x,y))
-                    visited.add((x,y))
-            num_mins += 1
+                    new_x, new_y = x + dx, y + dy
+                    
+                    if 0 <= new_x < len(grid) and \
+                    0 <= new_y < len(grid[0]) and \
+                    grid[new_x][new_y] == 1:
+                        grid[new_x][new_y] = 2
+                        q.append((new_x, new_y))
+            if len(q) > 0:
+                day += 1
         
-        #Check if there are any fresh oranges
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1:
+        # check if there are still any fresh oranges
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
                     return -1
-        return max(num_mins - 1, 0)
+        return day
+        
+        
