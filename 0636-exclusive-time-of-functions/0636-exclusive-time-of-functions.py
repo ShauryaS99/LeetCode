d116b024e8d142d37cd96ed51d2b532f68da39f8
ShauryaS99 <32023640+ShauryaS99@users.noreply.github.com>
2024-11-26 12:23:39 -0800

    Time: 5 ms (79.39%), Space: 17.4 MB (6.69%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,26 +1,22 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        #O(N) for time and space
-        function_lst = [0] * n
-        call_stack = []
-        prev_start = 0
+        res = [0] * n  # Result array to store exclusive time for each function
+        stack = []  # Stack to track active functions
+        prev_time = 0  # Keeps track of the last timestamp
+
         for log in logs:
-            function, call_type, ts = log.split(":")
-            function = int(function)
-            ts = int(ts)
-            # 2 types of calls
-            if call_type == "start":
-                # If start: add to call stack, calculate prev func time elapsed
-                if call_stack:
-                    last_call = call_stack[-1]
-                else:
-                    last_call = function
-                function_lst[last_call] += ts - prev_start
-                call_stack.append(function)
-                prev_start = ts
-            else:
-                # If end: pop from call stack, calculate func time elapsed
-                last_call = call_stack.pop()
-                function_lst[last_call] += ts - prev_start + 1
-                prev_start = ts + 1
-        return function_lst
+            func, f_type, ts = log.split(":")
+            func, ts = int(func), int(ts)
+
+            if f_type == "start":
+                # Update the current top of the stack's time
+                if stack:
+                    res[stack[-1]] += ts - prev_time
+                stack.append(func)
+                prev_time = ts
+            else:  # "end" case
+                # Pop the current function and update its exclusive time
+                res[stack.pop()] += ts - prev_time + 1
+                prev_time = ts + 1  # Move to the next timestamp
+
+        return res
